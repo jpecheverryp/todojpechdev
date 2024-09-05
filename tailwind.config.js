40d227b7eeb3ed3bdafa1b033c6e674f5802f491
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2024-09-05 19:59:24 -0400

    fixed tailwind config, added components directory to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/html/*.html"],
+  content: ["./views/html/*.html", "./views/html/components/*.html"],
   theme: {
     extend: {},
   },
