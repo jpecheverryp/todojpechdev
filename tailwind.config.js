a178f59e4eaea1dc3dc049956c23492ca886af77
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2024-09-05 20:03:11 -0400

    fixed styles add pages to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/html/*.html", "./views/html/components/*.html"],
+  content: ["./views/html/*.html", "./views/html/components/*.html", "./views/html/pages/*.html"],
   theme: {
     extend: {},
   },
